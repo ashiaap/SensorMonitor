4412986fa344da1bb64b2c2f4f7ee5a617488cdf
ashiaap <namaluffy86@gmail.com>
2019-03-28 14:21:13 +0700

    first commit

---
+++ app/src/main/java/com/example/sensormonitor/MainActivity.java
@@ -0,0 +1,133 @@
+package com.example.sensormonitor;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    private SensorManager sensorManager;
+    private Sensor sensorProximity;
+    private Sensor sensorAccelero;
+    private Sensor sensorTemp;
+    private Sensor sensorHumi;
+    private Sensor sensorLight;
+    private Sensor sensorPres;
+    private TextView txSensorProximity;
+    private TextView txSensorAccelero;
+    private TextView txSensorTemp;
+    private TextView txSensorHum;
+    private TextView txSensorLight;
+    private TextView txSensorPressure;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        sensorManager=(SensorManager)getSystemService(Context.SENSOR_SERVICE);
+        List<Sensor> sensorList=sensorManager.getSensorList(Sensor.TYPE_ALL);
+
+        StringBuilder sensoTtext =  new StringBuilder();
+        for(Sensor sensor:sensorList){
+            sensoTtext.append(sensor.getName())
+                    .append(System.getProperty("line.separator"));
+        }
+        //TextView sensorTextView=(TextView)findViewById(R.id.sensor_list);
+        //sensorTextView.setText(sensoTtext);
+
+        txSensorProximity=(TextView)findViewById(R.id.sensor_prox);
+        sensorProximity=sensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+        if(sensorProximity==null){
+            Toast.makeText(this,"no Proximity Sensor",Toast.LENGTH_SHORT).show();
+        }
+        txSensorAccelero=(TextView)findViewById(R.id.sensor_acc);
+        sensorAccelero=sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        if(sensorAccelero==null){
+            Toast.makeText(this,"no Accelero Sensor",Toast.LENGTH_SHORT).show();
+        }
+        txSensorTemp=(TextView)findViewById(R.id.sensor_temp);
+        sensorTemp=sensorManager.getDefaultSensor(Sensor.TYPE_AMBIENT_TEMPERATURE);
+        if(sensorTemp==null){
+            Toast.makeText(this,"no Temperature Sensor",Toast.LENGTH_SHORT).show();
+        }
+        txSensorHum=(TextView)findViewById(R.id.sensor_hum);
+        sensorHumi=sensorManager.getDefaultSensor(Sensor.TYPE_RELATIVE_HUMIDITY);
+        if(sensorHumi==null){
+            Toast.makeText(this,"no Temperature Sensor",Toast.LENGTH_SHORT).show();
+        }
+        txSensorLight=(TextView)findViewById(R.id.sensor_light);
+        sensorLight=sensorManager.getDefaultSensor(Sensor.TYPE_LIGHT);
+        if(sensorLight==null){
+            Toast.makeText(this,"no Light Sensor",Toast.LENGTH_SHORT).show();
+        }
+        txSensorPressure=(TextView)findViewById(R.id.sensor_press);
+        sensorPres=sensorManager.getDefaultSensor(Sensor.TYPE_PRESSURE);
+        if(sensorPres==null){
+            Toast.makeText(this,"no Pressure Sensor",Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        if(sensorProximity!=null){
+            sensorManager.registerListener(this,sensorProximity,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        }if(sensorAccelero!=null){
+            sensorManager.registerListener(this,sensorAccelero,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        }if(sensorTemp!=null){
+            sensorManager.registerListener(this,sensorTemp,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        }if(sensorHumi!=null){
+            sensorManager.registerListener(this,sensorHumi,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        }if(sensorLight!=null){
+            sensorManager.registerListener(this,sensorLight,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        }if(sensorPres!=null){
+            sensorManager.registerListener(this,sensorPres,
+                    SensorManager.SENSOR_DELAY_NORMAL);
+        }
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        sensorManager.unregisterListener(this);
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        int sensorType=sensorEvent.sensor.getType();
+        float value= sensorEvent.values[0];
+        if(sensorType==Sensor.TYPE_PROXIMITY){
+            txSensorProximity.setText("Proximity\n \n"+value);
+        }if(sensorType==Sensor.TYPE_ACCELEROMETER){
+            txSensorAccelero.setText("Accelerometer \n\n"+value);
+        }if(sensorType==Sensor.TYPE_AMBIENT_TEMPERATURE){
+            txSensorTemp.setText("Temperature \n\n"+value);
+        }if(sensorType==Sensor.TYPE_RELATIVE_HUMIDITY){
+            txSensorHum.setText("Humidity\n\n"+value);
+        }if(sensorType==Sensor.TYPE_LIGHT){
+            txSensorLight.setText("Light \n\n"+value);
+        }if(sensorType==Sensor.TYPE_PRESSURE){
+            txSensorPressure.setText("Pressure \n\n"+value);
+        }
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+}
